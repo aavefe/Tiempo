c1b9b670c8f3d9c013fb354d87a5b07e8701d730
Aaron <aavefe@fp.insjoaquimmir.cat>
2021-11-19 12:58:50 +0100

    Creadas tablas con switch

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 
 onload=function crearTablas(){
-    var contenedor = document.getElementById("contenedor");
 
-
-   
     var tblBody = document.createElement("tbody");
 
     var d = new Date();
 
     
     var horassiguientes = 24 - d.getHours();
-    
-    
-    for(var i=0; i<horassiguientes; i++){
-        
+    for(var i=0; i<horassiguientes; i++){     
         var hilera = document.createElement("tr");
-        
-        var textocelda = document.createTextNode(d.getHours()+i);
-       
-        
-        
         for(var j=0; j<5; j++){
             var celda = document.createElement("td");
-            if(j==0){
-                celda.appendChild(textocelda);
-            }
-            
-            
-            
+
+            switch(j){
+                case 0: celda.innerHTML = d.getHours()+i;
+                break;
+
+                case 1: celda.innerHTML = "Valor temperatura";
+                break;
+
+                case 2: celda.innerHTML = "Valor humedad";
+                break;
+
+                case 3: celda.innerHTML = "Valor tiempo";
+                break;
+
+                case 4: celda.innerHTML = "Valor viento";
+                break;
+            }   
             hilera.appendChild(celda);
             
         }
